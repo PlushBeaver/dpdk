27d7e2a594564898e5b2df2d6f07d1172ea3e5d0
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2020-01-31 06:43:30 +0300

    eal/windows: refine public interface
    The goal of rte_os.h is to mitigate OS differences for EAL users.
    In Windows EAL, rte_os.h did excessive things:
    
    1. It included platform SDK headers (windows.h, etc). Those files are
       huge, require specific inclusion order, and are generally unused by
       the code including rte_os.h. Declarations from platform SDK may
       break otherwise platform-independent code, e.g. min, max, ERROR.
    
    2. It included pthread.h, which is clearly not always required.
    
    3. It defined functions private to Windows EAL.
    
    Reorganize Windows EAL includes in the following way:
    
    1. Create rte_windows.h to properly import Windows-specific facilities.
       Primary users are bus drivers, tests, and external applications.
    
    2. Remove platform SDK includes from rte_os.h to prevent breaking
       otherwise portable code by including rte_os.h on Windows.
       Copy necessary definitions to avoid including those headers.
    
    3. Remove pthread.h include from rte_os.h.
    
    4. Move declarations private to Windows EAL into eal_windows.h.
    
    Fixes: 428eb983 ("eal: add OS specific header file")
    
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>

---
+++ lib/librte_eal/windows/eal/include/rte_windows.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2020 Dmitry Kozlyuk
+ */
+
+#ifndef _RTE_WINDOWS_H_
+#define _RTE_WINDOWS_H_
+
+/**
+ * @file Windows-specific facilities
+ *
+ * This file should be included by DPDK libraries and applications
+ * that need access to Windows API. It includes platform SDK headers
+ * in compatible order with proper options and defines error-handling macros.
+ */
+
+/* Disable excessive libraries. */
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+/* Must come first. */
+#include <windows.h>
+
+#include <basetsd.h>
+#include <psapi.h>
+
+/* Have GUIDs defined. */
+#ifndef INITGUID
+#define INITGUID
+#endif
+#include <initguid.h>
+
+/**
+ * Log GetLastError() with context, usually a Win32 API function and arguments.
+ */
+#define RTE_LOG_WIN32_ERR(format, ...) \
+	do { \
+		RTE_LOG(DEBUG, EAL, "GetLastError()=%lu: " format "\n", \
+			GetLastError(), ##__VA_ARGS__); \
+	} while (0)
+
+#endif /* _RTE_WINDOWS_H_ */
