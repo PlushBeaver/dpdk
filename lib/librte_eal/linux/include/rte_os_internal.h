fa371ba36a5a9bb94c55ef71ad8a8ba3e983b85f
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-03-04 00:07:58 +0300

    eal: make OS shims internal
    DPDK code often relies on functions and macros that are not standard C,
    but are found on all platforms, even if by slightly different names.
    Windows <rte_os.h> provided macros or inline definitions for such symbols.
    However, when placed in public header, these symbols were unnecessarily
    exposed, breaking consumer POSIX compatibility code.
    
    Move all shims to <rte_os_internal.h>, a header to be used instead of
    <rte_os.h> by internal code. Include it in libraries and PMDs that
    previously imported shims from <rte_os.h>.
    
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>

---
+++ lib/librte_eal/linux/include/rte_os_internal.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#ifndef _RTE_OS_INTERNAL_H_
+#define _RTE_OS_INTERNAL_H_
+
+#include <rte_os.h>
+
+/**
+ * @file
+ * @internal
+ * Provides semi-standard OS facilities by convenient names.
+ */
+
+#endif /* _RTE_OS_INTERNAL_H_ */
